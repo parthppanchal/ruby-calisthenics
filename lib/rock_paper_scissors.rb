344859300f6f12b5b3437742025e710812f1a852
Parth Panchal <parthppanchal@ymail.com>
2014-11-30 02:16:49 -0400

    Implement RockPaperScissors Game Winner

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,7 +4,13 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+  	error = false
+    [player1, player2].each do |player|
+    	error |= !["R", "P", "S"].include?(player[1])
+    end
+		raise NoSuchStrategyError, "Strategy must be one of R,P,S" if error
+		win = {"R" => "S", "S" => "P", "P" => "R"}
+		{true => player2, false => player1}[win[player2[1]] == player1[1]]
   end
 
   def self.tournament_winner(tournament)
